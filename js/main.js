d5e3a641aaca907629cb06dc23480c76735ece02
Thuong Ho <thuong.t.ho@gmail.com>
2016-04-08 15:39:57 -0700

    update profile

--- js/main.js
+++ js/main.js
@@ -1,86 +1,113 @@
-(function(){
-  $(document).ready(function(){
 
-    // unlock the vault
-    $('.vault').on('click', function() {
-    $('#cover').css('background-image', 'url(../images/cosmic.jpg)').fadeOut("slow",function() {
-        $(this).fadeIn();
-      });
-      $('#cover').css('background-position', 'fixed');
-    });
+function main() {
 
-    // ********* hide navbar *********
-    // credits to Marius Craciunoiu
-    // *******************************
-    var scrolling;
-    var lastScrollTop = 0,
-        scrollTrigger = 5,
-        navbarHeight = $('nav').outerHeight();
-
-    // set scrolling to true if user scrolls
-    $(window).scroll(function(e) {
-      scrolling = true;
-    });
-
-    // run hideOrShow function when scrolling is detected
-    setInterval(function() {
-      if (scrolling) {
-        hideOrShowNavbar();
-        scrolling = false;
-      }
-    }, 250);
-
-    function hideOrShowNavbar() {
-      var scrollPosition = $(this).scrollTop();
-      // ******** Stats ********
-      // console.log('lastST' + lastScrollTop);
-      // console.log('scrollP' + scrollPosition);
-      // console.log('navbarH' + navbarHeight);
-      // console.log('winH' + $(window).height());
-      // console.log('docH' + $(document).height());
-      // console.log('----------------------------');
-
-      if (Math.abs(lastScrollTop - scrollPosition) <= scrollTrigger) {
-        return;
-      }
-
-      // scroll down and scroll past navbar height
-      if ((scrollPosition > lastScrollTop) && (scrollPosition > navbarHeight)) {
-        // hide navbar
-        $('nav').addClass('nav-up');
-      } else {
-        if ((scrollPosition + $(window).height()) < $(document).height()) {
-          $('nav').removeClass('nav-up');
+(function () {
+   'use strict';
+   
+   // Testimonial slider
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
         }
+      });
+
+  	$(document).ready(function() {
+  	    $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
+	
+	// affix the navbar after scroll below header
+$('#nav').affix({
+      offset: {
+        top: $('header').height()
       }
-      lastScrollTop = scrollPosition;
-    }
-    // ********* end hide navbar *********
+});	
 
-    // ********* make navbar links active *********
-    // On page load or screen resize, grap window top of Home, About, Portfolio, Links
-    // Give each one a number like 1, 2, 3, 4 and make checks to that number
-    // SP 0 Home 868 About 1311 Portfolio 2161 Links
-    // var $home = $('#cover'),
-    //     $about = $('#about'),
-    //     $portfolio = $('#portfolio'),
-    //     $links = $('#links');
-    // var lastActiveClass = $home,
-    //     currentClass;
+	// skills chart
+	$(document).ready(function(e) {
+	//var windowBottom = $(window).height();
+	var index=0;
+	$(document).scroll(function(){
+		var top = $('#skills').height()-$(window).scrollTop();
+		// console.log(top)
+		if(top<-1000){
+			if(index==0){	
+			
+				$('.chart').easyPieChart({
+					easing: 'easeOutBounce',
+					onStep: function(from, to, percent) {
+						$(this.el).find('.percent').text(Math.round(percent));
+					}
+				});
+			
+				}
+			index++;
+		}
+	})
+	//console.log(nagativeValue)
+	});
 
-    // // Home
-    // if (scrollPosition < 868) {
-    //   currentClass = $home;
-    // } else if ((scrollPosition >= 868) && (scrollPosition < 1311)) {
-    //   currentClass = $about;
-    // } else if ((scrollPosition >= 1311) && (scrollPosition < 2161))
 
-    // function checkLastActiveClass(activeClass) {
-    //   if (activeClass !== lastActiveClass) {
-    //     lastActiveClass = activeClass;
-    //   }
-    //   return lastActiveClass;
-    // }
-    // ********* end navbar links active *********
-  });  // end doc ready
-}());  // end iffy
+  	// Portfolio isotope filter
+    $(window).load(function() {
+        var $container = $('.portfolio-items');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+	
+	
+	// counterUp
+
+
+	$(document).ready(function( $ ) {
+		if($("span.count").length > 0){	
+			$('span.count').counterUp({
+					delay: 10, // the delay time in ms
+			time: 1000 // the speed time in ms
+			});
+		}
+	});
+
+  	// Pretty Photo
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		social_tools: false
+	});	
+
+}());
+
+
+}
+main();
